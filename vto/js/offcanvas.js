b3d99f50fcb49ee8dfee6d22ee32825e65f4425d
Kelly King <89489915+worktheweek@users.noreply.github.com>
2022-08-01 13:18:09 -0600

    vto js

---
+++ vto/js/offcanvas.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  });
+  document.querySelector('#navbarSideCollapse2').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  });
+  document.querySelector('#navbarSideCollapse3').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
